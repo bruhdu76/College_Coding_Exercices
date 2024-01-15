da6f08334e4829681b8837eb26790aec5d6f3416
bruhdu76 <114436406+bruhdu76@users.noreply.github.com>
2024-01-15 08:25:22 +0100

    Update Nombre_deviner_machine.py
    - Ajout de la variable nb_operations pour voir le nombre d'opérations réalisés par la machine.

--- Nombre_deviner_machine.py
+++ Nombre_deviner_machine.py
@@ -8,16 +8,19 @@ def deviner_nombre_inverser() -> None:
     """
     borne_min = 0
     borne_max = 1001
+    nb_operatons = 0
     nombre_utilisateur = int(input("Veuillez choisir le nombre que la machine devra deviner : "))
     nombre_machine = randint(borne_min, borne_max)
     while nombre_machine != nombre_utilisateur:
         if nombre_machine < nombre_utilisateur:
             borne_min = nombre_machine
             nombre_machine = randint(borne_min, borne_max)
+            nb_operatons += 1
         elif nombre_machine > nombre_utilisateur:
             borne_max = nombre_utilisateur
             nombre_machine = randint(borne_min, borne_max)
-    return print(f"Bonne réponse ! {nombre_machine}")
+            nb_operatons += 1
+    return print(f"Bonne réponse ! |{nombre_machine}|, le nombre d'opérations est de |{nb_operatons}|")
 
 
 if __name__ == "__main__":
