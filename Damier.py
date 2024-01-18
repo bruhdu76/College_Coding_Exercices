7d1e779b99a0e72d64e522e12e8640fe6218d7a2
bruhdu76 <114436406+bruhdu76@users.noreply.github.com>
2024-01-18 15:15:09 +0100

    Update Damier.py
    - Correction du programme ne pouvant pas faire de damier impair

--- Damier.py
+++ Damier.py
@@ -3,15 +3,15 @@ def damier() -> None:
     Crée un damier en fonction de la taille fournie par l'utilisateur
     """
     taille = int(input("Veuillez donner la taille de votre damier : "))
-    if taille % 2 == 0:
-        for i in range(taille):
-            if i % 2 == 0:
-                print("⬜⬛" * int(taille / 2))
-            elif i % 2 == 1:
-                print("⬛⬜" * int(taille / 2))
-    if taille % 2 != 0:
-        print("On ne peut pas créer un damier impair")
+    for ligne in range(taille):
+        for colonne in range(taille):
+            if (ligne + colonne) % 2:
+                print("⬜", end="")
+            else:
+                print("⬛", end="")
+        print()
 
 
 if __name__ == "__main__":
     print(damier())
+    
